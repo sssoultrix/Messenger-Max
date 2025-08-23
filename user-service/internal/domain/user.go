bd80a451dfc7c64a8b4c43fce8bee279322e29d8
sssoultrix <gekpro22848pro@mail.com>
2025-08-23 11:56:12 +0300

    Created repository with comments

--- user-service/internal/domain/user.go
+++ user-service/internal/domain/user.go
@@ -4,13 +4,20 @@ import "time"
 
 type User struct {
 	ID           int64     `json:"id"`
-	Login        string    `json:"Login"`
+	Name         string    `json:"name"`
+	Login        string    `json:"login"`
 	PasswordHash string    `json:"password_hash"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 type UserCreateRequest struct {
+	Name     string `json:"name"`
 	Login    string `json:"login"`
 	Password string `json:"password"`
+}
+
+type UserResponse struct {
+	Name  string `json:"name"`
+	Login string `json:"login"`
 }
--- user-service/internal/service/user_service.go
+++ user-service/internal/service/user_service.go
@@ -5,11 +5,12 @@ import (
 	"messenger-max/user-service/internal/domain"
 )
 
+// Описание контракта для любого репозитория который будет работать с user_service
 type Repository interface {
 	Create(ctx context.Context, request domain.UserCreateRequest) error
 	Update(ctx context.Context, request domain.UserCreateRequest) error
 	Delete(ctx context.Context, id int64) error
-	GetByID(ctx context.Context, id int64) (domain.User, error)
-	GetAll(ctx context.Context) ([]domain.User, error)
-	GetByLogin(ctx context.Context, login string) (domain.User, error)
+	GetByID(ctx context.Context, id int64) (*domain.User, error)
+	GetAll(ctx context.Context) ([]*domain.User, error)
+	GetByLogin(ctx context.Context, login string) (*domain.User, error)
 }
