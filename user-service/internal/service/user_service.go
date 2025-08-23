f949c0fcdeba4e1461202e3ea5e078307cc0559a
sssoultrix <gekpro22848pro@mail.com>
2025-08-23 11:59:10 +0300

    Created base service for repo

--- user-service/internal/service/user_service.go
+++ user-service/internal/service/user_service.go
@@ -14,3 +14,35 @@ type Repository interface {
 	GetAll(ctx context.Context) ([]*domain.User, error)
 	GetByLogin(ctx context.Context, login string) (*domain.User, error)
 }
+
+type UserService struct {
+	repo Repository
+}
+
+func NewUserService(repo Repository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (u *UserService) Create(ctx context.Context, request domain.UserCreateRequest) error {
+	return u.repo.Create(ctx, request)
+}
+
+func (u *UserService) Update(ctx context.Context, request domain.UserCreateRequest) error {
+	return u.repo.Update(ctx, request)
+}
+
+func (u *UserService) Delete(ctx context.Context, id int64) error {
+	return u.repo.Delete(ctx, id)
+}
+
+func (u *UserService) GetByID(ctx context.Context, id int64) (*domain.User, error) {
+	return u.repo.GetByID(ctx, id)
+}
+
+func (u *UserService) GetAll(ctx context.Context) ([]*domain.User, error) {
+	return u.repo.GetAll(ctx)
+}
+
+func (u *UserService) GetByLogin(ctx context.Context, login string) (*domain.User, error) {
+	return u.repo.GetByLogin(ctx, login)
+}
