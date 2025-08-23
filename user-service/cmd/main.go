9324b7cd1b259d1b2c7732c3f407f209f3eb5832
sssoultrix <gekpro22848pro@mail.com>
2025-08-23 18:07:03 +0300

    maked cfg, docker, migration

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,1 +1,16 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"messenger-max/user-service/pkg/config"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+	log.Println(dsn)
+}
--- user-service/pkg/config/config.go
+++ user-service/pkg/config/config.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBHost     string
+	DBPort     string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load("user-service/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := &Config{
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "user_db"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+	}
+
+	return cfg
+}
+
+// Вспомогательная функция: возвращает значение переменной окружения или дефолт
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
